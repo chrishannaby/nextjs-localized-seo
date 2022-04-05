22f6539008e7f7f34662eedb746cf44b9c8f4172
Chris Hannaby <chris.hannaby@netlify.com>
2022-04-05 15:34:35 -0600

    add basic SEO info

--- pages/[state]/[city].js
+++ pages/[state]/[city].js
@@ -1,5 +1,6 @@
 import ProductDetails from "../../components/ProductDetails";
 import { getCity, getTopCities } from "../../util/cities.js";
+import { NextSeo } from "next-seo";
 
 export async function getStaticPaths() {
   const topCites = getTopCities();
@@ -32,5 +33,13 @@ export async function getStaticProps({ params }) {
 }
 
 export default function Home({ city }) {
-  return <ProductDetails city={city} />;
+  return (
+    <>
+      <NextSeo
+        title={`Basic Tee 6-Pack - ${city.city}, ${city.state}`}
+        description={`The Basic Tee 6-Pack allows you to fully express your vibrant personality with three grayscale options. Feeling adventurous? Put on a heather gray tee. Want to be a trendsetter? Try our exclusive colorway: "Black". Need to add an extra pop of color to your outfit? Our white tee has you covered. Available in ${city.city}, ${city.state}}`}
+      />
+      <ProductDetails city={city} />
+    </>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 import ProductDetails from "../components/ProductDetails";
 
 export default function Home() {
-  return <ProductDetails />;
+  return (
+    <>
+      <NextSeo
+        title={`Basic Tee 6-Pack`}
+        description={`The Basic Tee 6-Pack allows you to fully express your vibrant personality with three grayscale options. Feeling adventurous? Put on a heather gray tee. Want to be a trendsetter? Try our exclusive colorway: "Black". Need to add an extra pop of color to your outfit? Our white tee has you covered.`}
+      />
+      <ProductDetails />
+    </>
+  );
 }
